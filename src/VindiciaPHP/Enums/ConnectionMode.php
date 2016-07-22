a863a9ca6aabfa3ee072dba86da2e5c94586f1c1
Sam Waters <sam@samwaters.com>
2016-07-22 18:22:16 +0100

    Connection mode enum and fetch chargebacks request

---
+++ src/VindiciaPHP/Enums/ConnectionMode.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @author Sam Waters <sam@samwaters.com>
+ * @created 22/07/2016 18:12
+ */
+namespace VindiciaPHP\Enums;
+
+class ConnectionMode
+{
+  const LIVE = "live";
+  const TEST = "test";
+}
